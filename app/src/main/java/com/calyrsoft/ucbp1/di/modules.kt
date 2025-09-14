53ccfe1d58759eefe6b75fa900145d2280d81f21
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-13 20:44:49 -0400

    feat: login (1)

--- app/src/main/java/com/calyrsoft/ucbp1/di/modules.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/di/modules.kt
@@ -4,6 +4,9 @@ import com.calyrsoft.ucbp1.features.github.data.repository.GithubRepository
 import com.calyrsoft.ucbp1.features.github.domain.repository.IGithubRepository
 import com.calyrsoft.ucbp1.features.github.domain.usecase.FindByNickNameUseCase
 import com.calyrsoft.ucbp1.features.github.presentation.GithubViewModel
+import com.calyrsoft.ucbp1.features.login.data.repository.LoginRepository
+import com.calyrsoft.ucbp1.features.login.domain.usecase.LoginUseCase
+import com.calyrsoft.ucbp1.features.login.presentation.LoginViewModel
 import com.calyrsoft.ucbp1.features.profile.application.ProfileViewModel
 import com.calyrsoft.ucbp1.features.profile.data.repository.ProfileRepository
 import com.calyrsoft.ucbp1.features.profile.domain.repository.IProfileRepository
@@ -12,13 +15,15 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+    single { LoginRepository() }
+    factory { LoginUseCase(get()) }
+    viewModel { LoginViewModel(get()) }
+
     single<IGithubRepository>{ GithubRepository() }
     factory { FindByNickNameUseCase(get()) }
     viewModel { GithubViewModel(get()) }
 
-    viewModel { ProfileViewModel(get()) }
-    factory { GetProfileUseCase(get()) }
     single<IProfileRepository> { ProfileRepository() }
-
-
+    factory { GetProfileUseCase(get()) }
+    viewModel { ProfileViewModel(get()) }
 }
--- app/src/main/java/com/calyrsoft/ucbp1/navigation/AppNavigation.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/navigation/AppNavigation.kt
@@ -6,7 +6,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.calyrsoft.ucbp1.features.forgot_password.presentation.ForgotPasswordScreen
 import com.calyrsoft.ucbp1.features.github.presentation.GithubScreen
+import com.calyrsoft.ucbp1.features.login.presentation.LoginScreen
 import com.calyrsoft.ucbp1.features.profile.application.ProfileScreen
 
 @Composable
@@ -15,8 +17,22 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Profile.route
+        startDestination = Screen.Login.route
     ) {
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onForgotPasswordClicked = {
+                    navController.navigate(Screen.ForgotPassword.route)
+                }
+            )
+        }
+        composable(Screen.ForgotPassword.route) {
+            ForgotPasswordScreen(
+                onBackClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
         composable(Screen.Github.route) {
             GithubScreen(modifier = Modifier)
         }
